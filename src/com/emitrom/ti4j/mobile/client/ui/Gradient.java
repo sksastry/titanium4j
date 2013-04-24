55b6db8cfd1981e199a0c973c20deaedea15678e
Michael van Niekerk <mvniekerk@gmail.com>
2013-04-24 02:19:26 +0200

    Implement a gradient
    For use in view backgrounds

---
+++ src/com/emitrom/ti4j/mobile/client/ui/Gradient.java
@@ -0,0 +1,100 @@
+package com.emitrom.ti4j.mobile.client.ui;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+
+/**
+ * A gradient js object, used in backgrounds for views
+ *
+ * mvniekerk
+ *
+ */
+public class Gradient extends JavaScriptObject {
+	
+	protected Gradient() {
+		setAsLinear();
+		setStartPoint("0", "0");
+		setEndPoint("0", "0");
+		String[] colors = {"white"};
+		double[] offsets = {0};
+		setColors(colors, offsets);
+	}
+
+	/**
+	 * Set the gradient type as linear
+	 */
+	public native final void setAsLinear() /*-{
+		this.type = 'linear';
+	}-*/;
+	
+	/**
+	 * Set the gradient type as radial
+	 */
+	public native final void setAsRadial() /*-{
+		this.type = 'radial';
+	}-*/;
+	
+	/**
+	 * Set the start point of the gradient
+	 * @param x
+	 * @param y
+	 */
+	public native final void setStartPoint(String x, String y) /*-{
+		var sp = {};
+		sp.x = x;
+		sp.y = y;
+		this.startPoint = sp;
+	}-*/;
+	
+	/**
+	 * Set the end point of the gradient
+	 * @param x
+	 * @param y
+	 */
+	public native final void setEndPoint(String x, String y) /*-{
+		var sp = {};
+		sp.x = x;
+		sp.y = y;
+		this.endPoint = sp;
+	}-*/;
+	
+	/**
+	 * Set the colors of gradient. See createColorOffset code
+	 * for reference JSO
+	 * @param colors
+	 */
+	public native final void setColors(JsArray<JavaScriptObject> colors) /*-{
+		this.colors = colors;
+	}-*/;
+	
+	/**
+	 * Helper function creating a color and its offset.
+	 * Used when setting
+	 * @param color		The color
+	 * @param offset	And its offset
+	 * @return			The jso of this color + offset
+	 */
+	public native final JavaScriptObject createColorOffset(String color, double offset) /*-{
+		var ret = {};
+		ret.color = color;
+		ret.offset = offset;
+		return ret;
+	}-*/;
+	
+	/**
+	 * Set the colors of this gradient using an array of colors with equal amount
+	 * offsets and colors.
+	 * @param colors		The array of color strings
+	 * @param offsets		The offsets
+	 */
+	public void setColors(String[] colors, double[] offsets) {
+		if (colors.length != offsets.length) {
+			return; //Misfit between colors and offsets array length
+		}
+		@SuppressWarnings("unchecked")
+		JsArray<JavaScriptObject> c = (JsArray<JavaScriptObject>) JavaScriptObject.createArray();
+		for (int i = 0; i < colors.length; ++i) {
+			c.set(i, createColorOffset(colors[i], offsets[i]));
+		}
+	}
+}
